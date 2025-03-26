7673ae25f87343ce8fb4109177695286808a0a38
Pablo Gargallo <pablogargallosanz@gmail.com>
2025-03-26 13:03:12 +0100

    Add: Enlace con bases datos

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,16 @@
+
+
+import java.sql.Connection;
 public class Menu {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    
+    
+    public static void main(String[] args) {
+        Connection conexion = ConexionDB.conectar();
+        if (conexion != null) {
+            System.out.println("Conexion establecida correctamente.");
+        } else{
+            System.out.println("No se pudo establecer la conexión");
+        }
     }
 }
