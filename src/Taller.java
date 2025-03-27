29cc93a67308c7504e6de2f066922b7a6e67ee2a
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-03-27 08:47:56 +0100

    Add: Creando Taller.java y métodos
    Se creó el archivo Taller.java que se utilizará para toda la lógica y
    métodos que se utilicen en los menús de Menu.java. Además se crearon los
    métodos para saber cuáles hay que hacer.

---
+++ src/Taller.java
@@ -0,0 +1,66 @@
+public class Taller {
+    
+    public void registrarCliente() {}
+    public void modificarCliente() {}
+    public void eliminarCliente() {}
+    public void mostrarClientes() {}
+
+
+    public void registrarVehiculo() {}
+    public void modificarVehiculo() {}
+    public void eliminarVehiculo() {}
+    public void mostrarVehiculos() {}
+
+
+    public void asignarCita() {}
+    public void modificarCita() {}
+    public void eliminarCita() {}
+    public void mostrarCitas() {}
+
+
+    public void registrarEncargo() {}
+    public void modificarEncargo() {}
+    public void finalizarEncargo() {}
+    public void eliminarEncargo() {}
+    public void mostrarEncargos() {}
+
+
+    public void registrarEmpleado() {}
+    public void modificarEmpleado() {}
+    public void eliminarEmpleado() {}
+    public void mostrarServiciosEmpleado() {}
+    public void revisarDisponibilidadEmpleado() {}
+    public void mostrarEmpleados() {}
+
+
+    public void asignarEmpleados() {}
+    public void modificarAsignacion() {}
+    public void eliminarAsignacion() {}
+    public void mostrarAsignaciones() {}
+
+
+    public void registrarServicio() {}
+    public void modificarServicio() {}
+    public void eliminarServicio() {}
+    public void mostrarServicios() {}
+
+
+    public void registrarItem() {}
+    public void modificarItem() {}
+    public void eliminarItem() {}
+    public void mostrarItems() {}
+
+
+    public void registrarPedido() {}
+    public void modificarPedido() {}
+    public void eliminarPedido() {}
+    public void mostrarPedidos() {}
+
+
+    public void registrarProveedor() {}
+    public void modificarProveedor() {}
+    public void eliminarProveedor() {}
+    public void mostrarProveedores() {}
+
+
+}
