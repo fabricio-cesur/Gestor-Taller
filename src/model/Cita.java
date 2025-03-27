d8e1ea4835b3587f2b7756dea852b0a7266a3bb3
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2025-03-27 22:53:41 +0100

    Feat: Métodos Citas
    Se crea los métodos para manejar citas cómo el registrar, modificar,
    eliminar y mostrar. Para ello se crearon getters, setters y toString
    para poder hacerlo.

--- src/model/Cita.java
+++ src/model/Cita.java
@@ -19,6 +19,20 @@ public class Cita {
     public String getFecha() { return this.fecha; }
     public String getHora() { return this.hora; }
     public Vehiculo getVehiculo() { return this.vehiculo; }
+    public String getVehiculoMarcaModelo() { return this.getVehiculo().getMarca() + " " + this.getVehiculo().getModelo(); }
     public Encargo getEncargo() { return this.encargo; }
 
+    public void setId(int id) { this.id = id; }
+    public void setFecha(String fecha) { this.fecha = fecha; }
+    public void setHora(String hora) { this.hora = hora; }
+    public void setVehiculo(Vehiculo vehiculo) { this.vehiculo = vehiculo; }
+    public void setEncargo(Encargo encargo) { this.encargo = encargo; }
+
+    @Override
+    public String toString() {
+        return this.getId() + ": " 
+        + this.getFecha() + " " + this.getHora() 
+        + " vehiculo: " + this.getVehiculoMarcaModelo() 
+        + " servicio: " + this.getEncargo().getServicio();
+    }
 }
