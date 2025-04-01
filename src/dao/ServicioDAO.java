6f45a28e9eefc81542a61988810320ff9a8b0705
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-04-01 12:42:55 +0200

    Feat: Insertar Servicio DAO

--- src/dao/ServicioDAO.java
+++ src/dao/ServicioDAO.java
@@ -1,5 +1,28 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Servicio;
+
 public class ServicioDAO {
+
+    public boolean insertar(Servicio servicio) {
+        Connection conexion = ConexionDB.conectar(); 
+        if (conexion != null) { 
+            String query = "INSERT INTO Empleado (nombre, descripcion, id_item, precio,) VALUES (" + servicio.getNombre() 
+            + ", " + servicio.getDescripcion() + ", " + servicio.getItem() + ", " +servicio.getPrecio() +");" ; 
+            try (PreparedStatement stmt = conexion.prepareStatement(query)) { 
+               
+                stmt.executeUpdate(); // Ejecuta la consulta de inserción 
+
+                return true; 
+            } catch (SQLException e) { 
+                System.out.println("Error al agregar servicio: " + e.getMessage()); 
+            } 
+        }
+        return false;
+    }
+
     
 }
--- src/model/Servicio.java
+++ src/model/Servicio.java
@@ -1,26 +1,24 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Servicio {
     public String id;
     public String nombre;
     public String descripcion;
-    public ArrayList<String> items;
+    public String item;
     public double precio;
 
-    public Servicio(String id, String nombre, String descripcion, ArrayList<String> items, double precio) {
+    public Servicio(String id, String nombre, String descripcion, String item, double precio) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.items = items;
+        this.item = item;
         this.precio = precio;
     }
 
     public String getId() { return this.id; }
     public String getNombre() { return this.nombre; }
     public String getDescripcion() { return this.descripcion; }
-    public ArrayList<String> getItems() { return this.items; }
+    public String getItem() { return this.item; }
     public double getPrecio() { return this.precio; }
 
 }
