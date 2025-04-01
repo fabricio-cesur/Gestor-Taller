eda03803ab7288fae2e0d7935508d1af60191592
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-04-01 14:21:32 +0200

    Feat: Proveedor buscar y obtener

--- src/dao/ProveedorDAO.java
+++ src/dao/ProveedorDAO.java
@@ -2,9 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import model.Proveedor;
-
 public class ProveedorDAO {
     public boolean insertar(Proveedor proveedor) {
         Connection conexion = ConexionDB.conectar(); 
@@ -54,5 +55,80 @@ public class ProveedorDAO {
             }
         }
         return false;
+    }
+
+    public String buscar(String id) {
+        Connection conexion = ConexionDB.conectar();
+        String id_busqueda = null;
+
+        if (conexion != null) {
+            String query = "SELECT * FROM Proveedor WHERE id = " + id;
+
+            try ( PreparedStatement stmt = conexion.prepareStatement(query)) {
+                
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    id_busqueda =  rs.getString("id");
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al buscar proveedor por ID: " + e.getMessage());
+            }
+            return id_busqueda; 
+        }
+        return null; 
+    }
+
+    public Proveedor buscarMostrar(String id) {
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            Proveedor proveedor = null;
+            String query = "SELECT * FROM Proveedor WHERE id = " + id;
+
+            try ( PreparedStatement stmt = conexion.prepareStatement(query)) {
+                
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    proveedor = new Proveedor(
+                        rs.getString("nombre"),
+                        rs.getString("direccion"),
+                        rs.getString("cuenta_bancaria")
+                    );
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al buscar proveedor por ID: " + e.getMessage());
+            }
+            return proveedor; 
+        }
+        return null;
+    }
+
+    public ArrayList<Proveedor> obtenerTodos() {
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            ArrayList<Proveedor> proveedores = new ArrayList<>();
+            String query = "SELECT * FROM Proveedor";
+
+            try (
+                PreparedStatement stmt = conexion.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery()) {
+                
+                while (rs.next()) {
+                    Proveedor proveedor = new Proveedor(
+                        rs.getString("nombre"),
+                        rs.getString("direccion"),
+                        rs.getString("cuenta_bancaria")
+                    );
+                    proveedores.add(proveedor);
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al obtener todos los proveedor: " + e.getMessage());
+            }
+            return proveedores;
+        }
+        return null;
     }
 }
