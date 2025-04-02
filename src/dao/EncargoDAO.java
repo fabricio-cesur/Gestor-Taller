63054e000fb8b172558ccdcfab06c5b6fc955878
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-04-02 09:41:37 +0200

    Feat: insertar y actualizar Encargo

--- src/dao/EncargoDAO.java
+++ src/dao/EncargoDAO.java
@@ -1,20 +1,16 @@
-/*
 package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import model.Encargo;
 
 public class EncargoDAO {
 
-    public boolean  insertar(Encargo encargo) {
+    public boolean insertar(Encargo encargo) {
         Connection conexion = ConexionDB.conectar(); 
         if (conexion != null) { 
-            String query = "INSERT INTO Encargo ( matricula_coche, precio_total, fecha_inicio, fecha_finalizado ) VALUES (" + encargo.getMatricula() + ", " 
-            + encargo.getPrecio_total() + ", " + encargo.fecha_inicio + ", " + encargo.getFechaFinalizado() + ");" ; 
+            String query = "INSERT INTO Encargo (matricula_coche, precio_total) VALUES (" + encargo.getMatricula() + ", " + encargo.getPrecioTotal() + ");";
             try (PreparedStatement stmt = conexion.prepareStatement(query)) { 
                
                 stmt.executeUpdate(); // Ejecuta la consulta de inserción 
@@ -27,10 +23,10 @@ public class EncargoDAO {
         return false;
     }
 
-    public boolean actualizar(String columna, String matricula, String valor ) {
+    public boolean actualizar(String columna, int id, String valor ) {
         Connection conexion = ConexionDB.conectar();
         if (conexion != null) {
-            String query = "UPDATE Encargo SET " + columna + "=" + valor + " WHERE matricula = " + matricula; 
+            String query = "UPDATE Encargo SET " + columna + "=" + valor + " WHERE id = " + id; 
             try (PreparedStatement stmt = conexion.prepareStatement(query)) {
                                 
                 int filas_afectadas = stmt.executeUpdate();
@@ -44,107 +40,106 @@ public class EncargoDAO {
         return false;
     }
 
-    public boolean  eliminar(String matricula) {
-        Connection conexion = ConexionDB.conectar();
-        if (conexion != null) {
-            String query = "DELETE FROM Encargo WHERE dni = " + matricula;
-            try (PreparedStatement stmt = conexion.prepareStatement(query)) {
+    // public boolean  eliminar(String matricula) {
+    //     Connection conexion = ConexionDB.conectar();
+    //     if (conexion != null) {
+    //         String query = "DELETE FROM Encargo WHERE dni = " + matricula;
+    //         try (PreparedStatement stmt = conexion.prepareStatement(query)) {
                 
-                int filas_afectadas = stmt.executeUpdate();
+    //             int filas_afectadas = stmt.executeUpdate();
                 
-                return filas_afectadas == 1;
-            } catch (SQLException e) {
-                System.out.println("Error al eliminar cliente: " + e.getMessage());
-            }
-        }
-        return false;
-    }
+    //             return filas_afectadas == 1;
+    //         } catch (SQLException e) {
+    //             System.out.println("Error al eliminar cliente: " + e.getMessage());
+    //         }
+    //     }
+    //     return false;
+    // }
 
-    public String buscar(String matricula) {
-        Connection conexion = ConexionDB.conectar();
-        String matricula_busqueda = null;
+    // public String buscar(String matricula) {
+    //     Connection conexion = ConexionDB.conectar();
+    //     String matricula_busqueda = null;
 
-        if (conexion != null) {
-            Encargo encargo = null;
-            String query = "SELECT * FROM Encargo WHERE matricula = " + matricula;
-            try ( PreparedStatement stmt = conexion.prepareStatement(query)) {
+    //     if (conexion != null) {
+    //         Encargo encargo = null;
+    //         String query = "SELECT * FROM Encargo WHERE matricula = " + matricula;
+    //         try ( PreparedStatement stmt = conexion.prepareStatement(query)) {
                 
-                ResultSet rs = stmt.executeQuery();
+    //             ResultSet rs = stmt.executeQuery();
 
-                if (rs.next()) {
-                    matricula_busqueda =  rs.getString("matricula");
+    //             if (rs.next()) {
+    //                 matricula_busqueda =  rs.getString("matricula");
                     
-                }
-            } catch (SQLException e) {
-                System.out.println("Error al buscar cliente por DNI: " + e.getMessage());
-            }
+    //             }
+    //         } catch (SQLException e) {
+    //             System.out.println("Error al buscar cliente por DNI: " + e.getMessage());
+    //         }
             
-            return matricula_busqueda; 
-        }
-        return null; 
-    }
+    //         return matricula_busqueda; 
+    //     }
+    //     return null; 
+    // }
 
-    public Encargo buscarMostrar(String matricula) {
-        Connection conexion = ConexionDB.conectar();
+    // public Encargo buscarMostrar(String matricula) {
+    //     Connection conexion = ConexionDB.conectar();
 
-        if (conexion != null) {
-            Encargo encargo = null;
-            String query = "SELECT * FROM Encargo WHERE matricula = " + matricula;
+    //     if (conexion != null) {
+    //         Encargo encargo = null;
+    //         String query = "SELECT * FROM Encargo WHERE matricula = " + matricula;
 
-            try ( PreparedStatement stmt = conexion.prepareStatement(query)) {
+    //         try ( PreparedStatement stmt = conexion.prepareStatement(query)) {
                 
-                ResultSet rs = stmt.executeQuery();
+    //             ResultSet rs = stmt.executeQuery();
 
-                if (rs.next()) {
-                    encargo = new Encargo(
-                        rs.getString("id"),
-                        rs.getString("matricula_vehiculo"),
-                        rs.getString("id_servicio"),
-                        rs.getDouble("precio_total"),
-                        rs.getString("id_asignacion"),
-                        rs.getString("fecha_inicio"),
-                        rs.getString("fecha_finalizado"),
-                        rs.getString("cod_item")
-                    );
-                }
-            } catch (SQLException e) {
-                System.out.println("Error al buscar cliente por DNI: " + e.getMessage());
-            }
-            return encargo; 
-        }
-        return null; 
-    }
+    //             if (rs.next()) {
+    //                 encargo = new Encargo(
+    //                     rs.getString("id"),
+    //                     rs.getString("matricula_vehiculo"),
+    //                     rs.getString("id_servicio"),
+    //                     rs.getDouble("precio_total"),
+    //                     rs.getString("id_asignacion"),
+    //                     rs.getString("fecha_inicio"),
+    //                     rs.getString("fecha_finalizado"),
+    //                     rs.getString("cod_item")
+    //                 );
+    //             }
+    //         } catch (SQLException e) {
+    //             System.out.println("Error al buscar cliente por DNI: " + e.getMessage());
+    //         }
+    //         return encargo; 
+    //     }
+    //     return null; 
+    // }
 
-    public ArrayList<Encargo> obtenerTodos() {
-        Connection conexion = ConexionDB.conectar();
-        ArrayList<Encargo> encargos = new ArrayList<>();
+    // public ArrayList<Encargo> obtenerTodos() {
+    //     Connection conexion = ConexionDB.conectar();
+    //     ArrayList<Encargo> encargos = new ArrayList<>();
 
-        if (conexion != null) {
+    //     if (conexion != null) {
             
-            String query = "SELECT * FROM Encargo";
+    //         String query = "SELECT * FROM Encargo";
 
-            try (
-                PreparedStatement stmt = conexion.prepareStatement(query);
-                ResultSet rs = stmt.executeQuery()) {
+    //         try (
+    //             PreparedStatement stmt = conexion.prepareStatement(query);
+    //             ResultSet rs = stmt.executeQuery()) {
                 
-                while (rs.next()) {
-                    Encargo encargo = new Encargo(
-                        rs.getString("id"),
-                        rs.getString("matricula_vehiculo"),
-                        rs.getString("id_servicio"),
-                        rs.getDouble("precio_total"),
-                        rs.getString("id_asignacion"),
-                        rs.getString("fecha_inicio"),
-                        rs.getString("fecha_finalizado"),
-                        rs.getString("cod_item")
-                    );
-                    encargos.add(encargo);
-                }
-            } catch (SQLException e) {
-                System.out.println("Error al obtener todos los clientes: " + e.getMessage());
-            }
-        }
-        return encargos;
-    }
+    //             while (rs.next()) {
+    //                 Encargo encargo = new Encargo(
+    //                     rs.getString("id"),
+    //                     rs.getString("matricula_vehiculo"),
+    //                     rs.getString("id_servicio"),
+    //                     rs.getDouble("precio_total"),
+    //                     rs.getString("id_asignacion"),
+    //                     rs.getString("fecha_inicio"),
+    //                     rs.getString("fecha_finalizado"),
+    //                     rs.getString("cod_item")
+    //                 );
+    //                 encargos.add(encargo);
+    //             }
+    //         } catch (SQLException e) {
+    //             System.out.println("Error al obtener todos los clientes: " + e.getMessage());
+    //         }
+    //     }
+    //     return encargos;
+    // }
 }
-*/
--- src/model/Encargo.java
+++ src/model/Encargo.java
@@ -2,15 +2,21 @@ package model;
 
 public class Encargo {
     public String matricula_vehiculo;
+    public Double precio_total;
     public String fecha_inicio;
+    public String fecha_finalizado;
     public boolean completado;
     
     
-    public Encargo(String matricula_vehiculo){
+    public Encargo(String matricula_vehiculo, Double precio_total){
         this.matricula_vehiculo = matricula_vehiculo;
-        // this.fecha_inicio = Citas.getDia(matricula_vehiculo)
+        this.precio_total = precio_total;
         this.completado = false;
     }
 
-    public String getMatricula() { return this.matricula_vehiculo; }       
+    public String getMatricula() { return this.matricula_vehiculo; } 
+    public Double getPrecioTotal() { return this.precio_total; }
+    public String getFechaInicio() { return this.fecha_inicio; }
+    public String getFechaFinalizado() { return this.fecha_finalizado; }
+    public boolean getCompletado() { return this.completado; }     
 }
