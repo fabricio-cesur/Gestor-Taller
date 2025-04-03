04b75c594979347ac7e5f0f3d68a500777c067f9
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-04-03 13:17:08 +0200

    Feat: modificar EncargoVIEW

--- src/view/EncargoVIEW.java
+++ src/view/EncargoVIEW.java
@@ -11,6 +11,7 @@ public class EncargoVIEW {
 
     public ArrayList<Encargo> array_encargos = new ArrayList<>();
     public EncargoDAO dao = new EncargoDAO();
+    public Formateo formateo = new Formateo();
     
     Scanner sc = new Scanner(System.in);
     public void registrar() {
@@ -43,6 +44,7 @@ public class EncargoVIEW {
                 num_servicios++;
             }
         } while (!id_servicio.equals("0") && num_servicios > 0);
+        dao.actualizar("precio_total", encargo.getId(), Double.toString(precio_total));
 
         System.out.println("El encargo empezará ahora?");
         System.out.println("1. SI / 2. NO");
@@ -60,119 +62,110 @@ public class EncargoVIEW {
 
         System.out.println("Encargo registrado.");
     }
-    // public void modificar() {
-    //     String opcion;
-    //     String dni;
-    //     String columna;
-    //     String valor;
+    public void modificar() {
+        String opcion;
+        String matricula;
+        String columna;
+        String valor;
 
-    //     Encargo encargo_modificar = null;
-    //     do {
-    //         System.out.println("Qué desea modificar?");
-    //         System.out.println("1. Dni");
-    //         System.out.println("2. Nombre");
-    //         System.out.println("3. Apellido");
-    //         System.out.println("4. Dirección");
-    //         System.out.println("5. Teléfono");
-    //         System.out.println("6. Cuenta Bancaria");
-    //         System.out.println("0. Atrás");
-    //         System.out.print(">>> ");
-    //         opcion = sc.nextLine();
-    //         //TODO: Validar que el DNI exista
-    //         System.out.println("Ingrese el DNI del encargo a modificar: ");
-    //         System.out.print("--> ");
-    //         dni = sc.nextLine();
+        Encargo encargo_modificar;
+        do {
+            System.out.println("Qué desea modificar?");
+            System.out.println("1. Matricula");
+            System.out.println("2. Servicios");
+            System.out.println("3. Fecha Inicio");
+            System.out.println("4. Completado");
+            System.out.println("0. Atrás");
+            System.out.print(">>> ");
+            opcion = sc.nextLine().toLowerCase();
+            //TODO: Validar que el Encargo exista
+            System.out.println("Ingrese la matrícula del encargo a modificar: ");
+            System.out.print("--> ");
+            matricula = sc.nextLine();
 
-    //         encargo_modificar = dao.buscarMostrar(dni);
+            encargo_modificar = dao.obtenerUltimo(matricula);
             
-    //         if (encargo_modificar == null) {
-    //             System.out.println("ERR0R: No se encontró el encargo");
-    //         } else {
-    //             switch (opcion) {
-    //                 //TODO: Añadir validaciones
-    //                 case "1", "dni" -> {
-    //                     System.out.print("Ingrese el nuevo DNI: ");
-    //                     String dni_nuevo = sc.next();
-    //                     valor = dni_nuevo;
-    //                     columna = "dni";
+            if (encargo_modificar == null) {
+                System.out.println("ERR0R: No se encontró el encargo");
+            } else {
+                switch (opcion) {
+                    //TODO: Añadir validaciones
+                    case "1", "matricula" -> {
+                        System.out.print("Ingrese la nueva matrícula: ");
+                        String matricula_nueva = sc.next();
+                        valor = matricula_nueva;
+                        columna = "matricula";
                         
-    //                     boolean actualizado = dao.actualizar(columna, dni, valor);
-    //                     if (actualizado) {
-    //                     System.out.println("DNI actualizado correctamente.");
-    //                     } else {
-    //                     System.out.println("Error al actualizar el DNI.");
-    //                     }
-
-    //                 }
-    //                 case "2", "nombre" -> {
-    //                     System.out.print("Ingrese el nuevo nombre: ");
-    //                     String nombre_nuevo = sc.next();
-    //                     columna = "nombre";
-    //                     valor = nombre_nuevo;
-    //                     boolean actualizado = dao.actualizar(columna, dni, valor);
-    //                     if (actualizado) {
-    //                         System.out.println("Nombre actualizado correctamente.");
-    //                     } else {
-    //                         System.out.println("Error al actualizar el nombre.");
-    //                     }
-    //                 }
-    //                 case "3", "apellido" -> {
-    //                     System.out.print("Ingrese el nuevo apellido: ");
-    //                     String apellido_nuevo = sc.next();
-    //                     columna = "apellido";
-    //                     valor = apellido_nuevo;
-    //                     boolean actualizado = dao.actualizar(columna, dni, valor);
-    //                     if (actualizado) {
-    //                         System.out.println("Apellido actualizado correctamente.");
-    //                     } else {
-    //                         System.out.println("Error al actualizar el apellido.");
-    //                     }
-    //                 }
-    //                 case "4", "direccion" -> {
-    //                     System.out.print("Ingrese la nueva dirección: ");
-    //                     String direccion_nueva = sc.nextLine();
-    //                     columna = "direccion";
-    //                     valor = direccion_nueva;
-    //                     boolean actualizado = dao.actualizar(columna, dni, valor);
-    //                     if (actualizado) {
-    //                         System.out.println("Dirección actualizada correctamente.");
-    //                     } else {
-    //                         System.out.println("Error al actualizar la dirección.");
-    //                     }
-    //                 }
-    //                 case "5", "telefono" -> {
-    //                     System.out.print("Ingrese el nuevo teléfono: ");
-    //                     int telefono_nuevo = sc.nextInt();
-    //                     columna = "telefono";
-    //                     valor = Integer.toString(telefono_nuevo); //paso el valor a String
-    //                     boolean actualizado = dao.actualizar(columna, dni, valor);
-    //                     if (actualizado) {
-    //                         System.out.println("Teléfono actualizado correctamente.");
-    //                     } else {
-    //                         System.out.println("Error al actualizar el teléfono.");
-    //                     }
-    //                 }
-    //                 case "6", "cuenta bancaria" -> {
-    //                     System.out.print("Ingrese la nueva cuenta bancaria: ");
-    //                     String cuenta_nueva = sc.nextLine();
-    //                     columna = "cuenta_bancaria";
-    //                     valor = cuenta_nueva;
-    //                     boolean actualizado = dao.actualizar(columna, dni, valor);
-    //                     if (actualizado) {
-    //                         System.out.println("Cuenta bancaria actualizada correctamente.");
-    //                     } else {
-    //                         System.out.println("Error al actualizar la cuenta bancaria.");
-    //                     }
-    //                 }
-    //                 default -> {
-    //                     System.out.println("ERR0R: No se reconoció esa opción");
-    //                 }
-    //             }
-    //         }
-
-    //     } while (!opcion.equalsIgnoreCase("0"));
-
-    // }
+                        if (dao.actualizar(columna, encargo_modificar.getId(), valor)) {
+                        System.out.println("DNI actualizado correctamente.");
+                        } else {
+                        System.out.println("Error al actualizar el DNI.");
+                        }
+                    }
+                    // case "2", "nombre" -> {
+                    //     System.out.print("Ingrese el nuevo nombre: ");
+                    //     String nombre_nuevo = sc.next();
+                    //     columna = "nombre";
+                    //     valor = nombre_nuevo;
+                    //     boolean actualizado = dao.actualizar(columna, id, valor);
+                    //     if (actualizado) {
+                    //         System.out.println("Nombre actualizado correctamente.");
+                    //     } else {
+                    //         System.out.println("Error al actualizar el nombre.");
+                    //     }
+                    // }
+                    case "3", "fecha incio", "fecha", "inicio" -> {
+                        System.out.println("El encargo empezará ahora?");
+                        System.out.println("1. SI / 2. NO");
+                        System.out.print("--> ");
+                        opcion = sc.next().toLowerCase();
+                        switch (opcion) {
+                            case "1", "si" -> {
+                                LocalDate fecha_inicio = LocalDate.now();
+                                columna = "fecha_inicio";
+                                valor = formateo.dateToString(fecha_inicio);
+                                if (dao.actualizar(columna, encargo_modificar.getId(), valor)) {
+                                    System.out.println("Fecha de inicio actualizada correctamente.");
+                                } else {
+                                    System.out.println("Error al actualizar la fecha de inicio.");
+                                }
+                            }
+                            case "2", "no" -> {/*TODO: Añadir mensaje?*/}
+                            default -> {
+                                System.out.println("No se reconoció esa opción");
+                            }
+                        }
+                    }
+                    case "4", "completado" -> {
+                        System.out.println("El encargo lo ha terminado?");
+                        System.out.println("1. SI / 2. NO");
+                        System.out.print("--> ");
+                        opcion = sc.next().toLowerCase();
+                        LocalDate fecha_finalizado;
+                        switch (opcion) {
+                            case "1", "si" -> {
+                                columna = "fecha_finalizado";
+                                fecha_finalizado = LocalDate.now();
+                                valor = formateo.dateToString(fecha_finalizado);
+                                if (dao.actualizar(columna, encargo_modificar.getId(), valor)) {
+                                    System.out.println("Encargo terminado el " + fecha_finalizado);
+                                } else {
+                                    System.out.println("Error al actualizar la fecha de inicio.");
+                                }
+                            }
+                            case "2", "no" -> {/*TODO: Añadir mensaje?*/}
+                            default -> {
+                                System.out.println("No se reconoció esa opción");
+                            }
+                        }
+                    }
+                    default -> {
+                        System.out.println("No se reconoció esa opción");
+                    }
+                }
+            }
+        } while (!opcion.equalsIgnoreCase("0"));
+    }
     // public void eliminar() {
     //     String dni;
     //     Encargo encargo = null;
